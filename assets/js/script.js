139b79d03741535d9ca88a64b1834da7aa5292ea
Youssef97dev <youssefmoussafi97@gmail.com>
2024-08-22 03:31:47 -1100

    change content 11

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,3 +183,14 @@ function filterItem(e) {
   });
   console.log(items.length);
 }
+
+let isChatboxVisible = false;
+const chatbox = document.querySelector(".WA_Chat_Widget .WA_ChatBox");
+function hideChatbox() {
+  isChatboxVisible = false;
+  chatbox.style.display = "none";
+}
+function toggleChatbox() {
+  isChatboxVisible = !isChatboxVisible;
+  chatbox.style.display = isChatboxVisible ? "block" : "none";
+}
