764e9fd8c2dfd44b0213cfca5779d69115fbbbd0
Youssef97dev <youssefmoussafi97@gmail.com>
2024-08-24 01:29:37 -1100

    change content 22

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,3 +194,18 @@ function toggleChatbox() {
   isChatboxVisible = !isChatboxVisible;
   chatbox.style.display = isChatboxVisible ? "block" : "none";
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
